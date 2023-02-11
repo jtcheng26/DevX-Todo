963716ee727e1e335fa6024858d41b9a55f02514
Howard Huang <howardhuang2004@gmail.com>
2023-02-10 20:47:30 -0800

    created PostgreSQL database to store tasks

---
+++ backend/database.sql
@@ -0,0 +1,7 @@
+-- CREATE TABLE TASKS(title TEXT, details TEXT, createDate TEXT, dueDate TEXT);
+
+-- INSERT INTO TASKS(title, details, createDate, dueDate) VALUES ('Physics HW', 'Chapter 29: Induction Mastering Physics', '2/9/2023', '2/10/2023');
+
+-- INSERT INTO TASKS(title, details, createDate, dueDate) VALUES ('CS32 Homework 3', 'Homework on virtual functions', '2/9/2023', '2/10/2023');
+
+SELECT * FROM TASKS;
